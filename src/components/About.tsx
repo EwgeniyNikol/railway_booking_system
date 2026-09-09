0282647968c2f5f60b0627f20e072d9665ca317f
ewgeny_nikol <ewg240@yandex.ru>
2026-09-09 13:48:46 +0300

    Add About section to HomePage

---
+++ src/components/About.tsx
@@ -0,0 +1,31 @@
+import styles from './About.module.scss';
+
+const About = () => {
+  return (
+    <section className={styles.about} id="about">
+      <h2 className={styles.about__title}>о нас</h2>
+      <div className={styles.about__content}>
+        <div className={styles.about__line}></div>
+        <div className={styles.about__text_block}>
+          <p className={styles.about__text}>
+            Мы рады видеть вас! Мы рботаем для Вас с 2003 года. 14 лет мы
+            наблюдаем, как с каждым днем все больше людей заказывают жд билеты
+            через интернет.
+          </p>
+          <p className={styles.about__text}>
+            Сегодня можно заказать железнодорожные билеты онлайн всего в 2
+            клика, но стоит ли это делать? Мы расскажем о преимуществах
+            заказа через интернет.
+          </p>
+          <p className={`${styles.about__text} ${styles.about__text_bold}`}>
+            Покупать жд билеты дешево можно за 90 суток до отправления поезда.
+            Благодаря динамическому ценообразованию цена на билеты в это время
+            самая низкая.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,11 @@
 import Header from '../components/Header';
-import styles from './HomePage.module.scss';
+import About from '../components/About';
 
 const HomePage = () => {
   return (
     <>
       <Header />
-      <main className={styles.home}>
-        <p className={styles.home__subtitle}>
-          Найдите и забронируйте билеты на поезд
-        </p>
-      </main>
+      <About />
     </>
   );
 };
